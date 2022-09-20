8fe7df259a1a2541029c595c702f84bc82b53719
Abraão Marques <4magein@gmail.com>
2022-09-20 09:16:47 -0300

    Adicionando máscara para CNPJ

--- view/frontend/web/js/script.js
+++ view/frontend/web/js/script.js
@@ -1,9 +1,12 @@
 require(
     [
         'jquery',
-        'AbraaoMarques_TaxvatValidator/js/jquery.maskedinput'
+        'AbraaoMarques_TaxvatValidator/js/jquery.inputmask.bundle'
     ],
     function(jQuery) {
-        jQuery("#taxvat").mask("999.999.999-99");
+        jQuery("#taxvat").inputmask({
+            mask: ['999.999.999-99', '99.999.999/9999-99'],
+            keepStatic: true
+        })
     }
 )
